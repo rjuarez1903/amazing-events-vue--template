76e24935e7b44220a79deffd28e3834b5c8377de
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-01-10 18:40:48 -0300

    add bootstrap cdn and upcoming events filter

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ createApp({
     data() {
         return {
             loading: true,
+            currentDate: '',
             title: document.title,
             events: [],
             filteredEvents: [],
@@ -17,8 +18,24 @@ createApp({
         fetch('https://mindhub-xj03.onrender.com/api/amazing')
             .then(response => response.json())
             .then(json => {
-                this.events = json.events
-                this.filteredEvents = this.events
+                this.currentDate = new Date(json['currentDate'])
+                console.log(this.currentDate)
+                this.events      = json.events
+                if (this.title == 'Home') {
+                    this.filteredEvents = this.events
+                } else if (this.title == 'Upcoming events') {
+                    this.filteredEvents = this.events.filter(event => {
+                        const eventDate = new Date(event.date)
+                        console.log(eventDate, this.currentDate)
+                        console.log(eventDate > this.currentDate)
+                        return eventDate > this.currentDate
+                    })
+                } else if (this.title == 'Past events') {
+                  this.filteredEvents = this.events.filter(event => {
+                      const eventDate = new Date(event.date)
+                      return eventDate < this.currentDate
+                  })
+                }
                 this.categories = [... new Set(json.events.map(event => event.category))]
             })
             .catch(err => console.log(err))
@@ -29,9 +46,6 @@ createApp({
     }, 
     methods: {
         crossFilter: function() {
-              console.log('Working')
-              console.log(this.checked)
-              console.log(this.title)
               let filteredByCategory = this.events.filter(event => this.checked.includes(event.category) || this.checked.length === 0)
               let filteredBySearch   = filteredByCategory.filter(event => event.name.toLowerCase().includes(this.inputSearch.toLowerCase()))
               this.filteredEvents = filteredBySearch
