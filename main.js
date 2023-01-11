1da7094080e8401675f6e3bf3079398601446b6c
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-01-10 23:08:05 -0300

    add new vars and comments

--- main.js
+++ main.js
@@ -3,15 +3,18 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            loading: true,
-            currentDate: '',
-            title: document.title,
-            events: [],
+            loading:        true,
+            // upcoming:       false,
+            // past:           false,
+            currentDate:    '',
+            title:           document.title,
+            upcomingEvents: [],
+            events:         [],
             filteredEvents: [],
-            categories: [],
-            bgGradient: '',
-            inputSearch: '',
-            checked: []
+            categories:     [],
+            bgGradient:     '',
+            inputSearch:    '',
+            checked:        []
         }
     },
     created() {
@@ -19,23 +22,23 @@ createApp({
             .then(response => response.json())
             .then(json => {
                 this.currentDate = new Date(json['currentDate'])
-                console.log(this.currentDate)
                 this.events      = json.events
-                if (this.title == 'Home') {
+                if (this.title.includes('Home')) {
                     this.filteredEvents = this.events
-                } else if (this.title == 'Upcoming events') {
-                    this.filteredEvents = this.events.filter(event => {
-                        const eventDate = new Date(event.date)
-                        console.log(eventDate, this.currentDate)
-                        console.log(eventDate > this.currentDate)
-                        return eventDate > this.currentDate
-                    })
-                } else if (this.title == 'Past events') {
-                  this.filteredEvents = this.events.filter(event => {
-                      const eventDate = new Date(event.date)
-                      return eventDate < this.currentDate
-                  })
-                }
+                } 
+                // else if (this.title == 'Upcoming events') {
+                //     this.filteredEvents = this.events.filter(event => {
+                //         this.upcoming = true
+                //         const eventDate = new Date(event.date)
+                //         return eventDate > this.currentDate
+                //     })
+                // } else if (this.title == 'Past events') {
+                //   this.filteredEvents = this.events.filter(event => {
+                //       this.past = true
+                //       const eventDate = new Date(event.date)
+                //       return eventDate < this.currentDate
+                //   })
+                // }
                 this.categories = [... new Set(json.events.map(event => event.category))]
             })
             .catch(err => console.log(err))
@@ -46,9 +49,10 @@ createApp({
     }, 
     methods: {
         crossFilter: function() {
+              // if (upcoming) {}
               let filteredByCategory = this.events.filter(event => this.checked.includes(event.category) || this.checked.length === 0)
               let filteredBySearch   = filteredByCategory.filter(event => event.name.toLowerCase().includes(this.inputSearch.toLowerCase()))
-              this.filteredEvents = filteredBySearch
+              this.filteredEvents    = filteredBySearch
           }
     }
 }).mount('#app')
