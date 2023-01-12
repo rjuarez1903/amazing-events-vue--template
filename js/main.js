566fe4c2a82b30cfbab041317a64969df29c3dae
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-01-12 17:45:06 -0300

    add filters for upcoming and past events

--- js/main.js
+++ js/main.js
@@ -4,41 +4,47 @@ createApp({
     data() {
         return {
             loading:        true,
-            // upcoming:       false,
-            // past:           false,
+            upcoming:       false,
+            past:           false,
+            title:          document.title,
             currentDate:    '',
-            title:           document.title,
+            bgGradient:     '',
+            inputSearch:    '',
             upcomingEvents: [],
+            pastEvents:     [],
             events:         [],
             filteredEvents: [],
             categories:     [],
-            bgGradient:     '',
-            inputSearch:    '',
-            checked:        []
+            checked:        [],
         }
     },
     created() {
         fetch('https://mindhub-xj03.onrender.com/api/amazing')
             .then(response => response.json())
             .then(json => {
-                this.currentDate = new Date(json['currentDate'])
-                this.events      = json.events
+                this.currentDate    = new Date(json['currentDate'])
+                this.events         = json.events
+                this.filteredEvents = this.events
                 if (this.title.includes('Home')) {
-                    this.filteredEvents = this.events
+                    this.upcoming = false
+                    this.past     = false
                 } 
-                // else if (this.title == 'Upcoming events') {
-                //     this.filteredEvents = this.events.filter(event => {
-                //         this.upcoming = true
-                //         const eventDate = new Date(event.date)
-                //         return eventDate > this.currentDate
-                //     })
-                // } else if (this.title == 'Past events') {
-                //   this.filteredEvents = this.events.filter(event => {
-                //       this.past = true
-                //       const eventDate = new Date(event.date)
-                //       return eventDate < this.currentDate
-                //   })
-                // }
+                else if (this.title.includes('Upcoming')) {
+                    this.upcomingEvents = this.events.filter(event => {
+                        this.upcoming   = true
+                        const eventDate = new Date(event.date)
+                        return eventDate > this.currentDate
+                    })
+                    this.filteredEvents = this.upcomingEvents
+                } else if (this.title.includes('Past')) {
+                    this.filteredEvents = this.events
+                    this.pastEvents     = this.events.filter(event => {
+                        this.past       = true
+                        const eventDate = new Date(event.date)
+                        return eventDate < this.currentDate
+                    })
+                    this.filteredEvents = this.pastEvents
+                }
                 this.categories = [... new Set(json.events.map(event => event.category))]
             })
             .catch(err => console.log(err))
@@ -49,10 +55,20 @@ createApp({
     }, 
     methods: {
         crossFilter: function() {
-              // if (upcoming) {}
-              let filteredByCategory = this.events.filter(event => this.checked.includes(event.category) || this.checked.length === 0)
-              let filteredBySearch   = filteredByCategory.filter(event => event.name.toLowerCase().includes(this.inputSearch.toLowerCase()))
-              this.filteredEvents    = filteredBySearch
-          }
+            if (this.upcoming) {
+                console.log(this.upcomingEvents)
+                let filteredByCategory = this.upcomingEvents.filter(event => this.checked.includes(event.category) || this.checked.length === 0)
+                let filteredBySearch   = filteredByCategory.filter(event => event.name.toLowerCase().includes(this.inputSearch.toLowerCase()))
+                this.filteredEvents    = filteredBySearch
+            } else if (this.past) {
+                let filteredByCategory = this.pastEvents.filter(event => this.checked.includes(event.category) || this.checked.length === 0)
+                let filteredBySearch   = filteredByCategory.filter(event => event.name.toLowerCase().includes(this.inputSearch.toLowerCase()))
+                this.filteredEvents    = filteredBySearch
+            } else {
+                let filteredByCategory = this.events.filter(event => this.checked.includes(event.category) || this.checked.length === 0)
+                let filteredBySearch   = filteredByCategory.filter(event => event.name.toLowerCase().includes(this.inputSearch.toLowerCase()))
+                this.filteredEvents    = filteredBySearch
+            }
+        }
     }
 }).mount('#app')
